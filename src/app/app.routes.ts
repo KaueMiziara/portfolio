1f930d58d0d7d6842cad40b9b40d14db402c73e5
Kaue Miziara <kauemiziara@gmail.com>
2025-12-22 02:17:16 -0300

    feat: define initial structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { About } from './features/about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: About }];
