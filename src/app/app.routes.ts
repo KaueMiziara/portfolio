bb75cbe0eea91052909ee8962380d49a4d10d2bd
Kaue Miziara <kauemiziara@gmail.com>
2025-12-23 17:22:42 -0300

    feat: initial screens and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,30 @@
 import { Routes } from '@angular/router';
 import { About } from './features/about/about';
 
-export const routes: Routes = [{ path: '', component: About }];
+export const routes: Routes = [
+  { path: '', component: About },
+  {
+    path: 'research',
+    async loadComponent() {
+      return import('./features/research/research').then((m) => m.Research);
+    },
+  },
+  {
+    path: 'projects',
+    async loadComponent() {
+      return import('./features/projects/projects').then((m) => m.Projects);
+    },
+  },
+  {
+    path: 'competitions',
+    async loadComponent() {
+      return import('./features/competitions/competitions').then((m) => m.Competitions);
+    },
+  },
+  {
+    path: 'community',
+    async loadComponent() {
+      return import('./features/community/community').then((m) => m.Community);
+    },
+  },
+];
