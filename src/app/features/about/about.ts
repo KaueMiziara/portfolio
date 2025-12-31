fd22823a4e6990c5079ff735d66d14240fae793e
Kaue Miziara <kauemiziara@gmail.com>
2025-12-31 14:29:35 -0300

    refactor: cleanup unused components and optimze operators

--- src/app/features/about/about.ts
+++ src/app/features/about/about.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { QuantumCard } from '../../shared/components/quantum-card/quantum-card';
-import { QuantumTag } from '../../shared/components/quantum-tag/quantum-tag';
 
 @Component({
   selector: 'app-about',
-  imports: [QuantumCard, QuantumTag],
+  imports: [QuantumCard],
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
